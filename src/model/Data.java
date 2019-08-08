46ed6edc0567b31b60adb8553ca893670f580b29
Parveen Dala <dparveen94@gmail.com>
2019-08-08 16:55:34 +0530

    Set model class

---
+++ src/model/Data.java
@@ -0,0 +1,21 @@
+package model;
+
+/*************
+ * Parveen D
+ * Cache Using RxJava
+ */
+public class Data {
+    private String source;
+
+    public Data(String source) {
+        this.source = source;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+}
